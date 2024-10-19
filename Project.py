74d5bdaab2791ed55b4eb5f938c0acd81ac19be8
gulu864 <suva.kanta4u@gmail.com>
2024-10-19 16:16:21 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,19 @@
+print("Welcome in here are are going to learn the tables of 2-5.")
+
+print("2 table----------")
+for i in range(2,22,2):
+    print(i)
+
+print("3 table----------")
+for j in range(3,33,3):
+    print(j)
+
+print("4 table----------")
+for x in range(4,44,4):
+    print(x)
+
+print("5 table----------")
+for y in range (5,55,5):
+    print(y)
+
+print("Now you know the tables 2 to 5.")
